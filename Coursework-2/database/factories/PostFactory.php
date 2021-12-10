b2baf8f8894b610cc485e7240cca238a15da67b5
Dan-975241 <975241@swansea.ac.uk>
2021-12-10 15:13:44 +0000

    Adding coursework 1 progress

---
+++ Coursework-2/database/factories/PostFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'postTitle' => $this->faker->sentence(1),
+            'postContent' => $this->faker->paragraph(2),
+            'user_id' => User::all()->random()->id,
+        ];
+    }
+}
---
+++ Coursework-2/database/seeders/PostTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+use App\Models\Post;
+use Illuminate\Database\Seeder;
+
+class PostTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $p = new Post;
+        $p->postTitle = "TestPostTitle";
+        $p->postContent = "TestPostContent";
+        $p->user_id = 1;
+        $p->save();
+
+        $posts = Post::factory()->count(15)->create();
+    }
+}
