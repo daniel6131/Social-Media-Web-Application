90cce928aa4d2cbe2d2d45ee09a6ddfd4a0b58af
Dan-975241 <975241@swansea.ac.uk>
2021-12-16 00:52:47 +0000

    Initial welcome page restructure

--- Coursework-2/resources/views/layouts/navigation.blade.php
+++ Coursework-2/resources/views/layouts/navigation.blade.php
@@ -1,6 +1,6 @@
 
 <nav class="flex items-center justify-between flex-wrap bg-indigo-500 p-6">
-    <a href="{{ route('dashboard') }}">
+    <a href="{{ url('/') }}">
         <div class="flex items-center text-white flex-shrink-0 mr-6">
             <svg class="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>
             <span class="font-semibold text-xl tracking-tight">ChitChat</span>
@@ -8,28 +8,39 @@
     </a>
     <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
         <div class="lg:flex-grow">
-            <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-indigo-200 hover:text-white mr-4">
-                Settings
-            </a>
-            <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-indigo-200 hover:text-white mr-4">
-                Notifications
-            </a>
-        </div>
-        <div>
-            <a href="{{ route('user.show', ['id' => Auth::user()->id]) }}" class="block mt-4 lg:inline-block lg:mt-0 text-indigo-200 hover:text-white mr-4">
-                My Profile
-            </a>
-        </div>
-        <div>
-            <form method="POST" action="{{ route('logout') }}">
-                @csrf
-
-                <a href="{{ route('logout') }}" onclick="event.preventDefault(); 
-                        this.closest('form').submit();"
-                        class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-indigo-500 hover:bg-white mt-4 lg:mt-0">
-                    {{ __('Log Out') }}
+        @if (Route::has('login'))
+            @auth
+                <a href="{{ route('dashboard') }}" class="block mt-4 lg:inline-block lg:mt-0 text-indigo-200 hover:text-white mr-4">
+                    Dashboard
                 </a>
-            </form>
+            @endauth
+        @endif
         </div>
+        @if (Route::has('login'))
+            @auth
+                <div>
+                    <a href="{{ route('user.show', ['id' => Auth::user()->id]) }}" class="block mt-4 lg:inline-block lg:mt-0 text-indigo-200 hover:text-white mr-4">
+                        My Profile
+                    </a>
+                </div>
+                <div>
+                    <form method="POST" action="{{ route('logout') }}">
+                        @csrf
+
+                        <a href="{{ route('logout') }}" onclick="event.preventDefault(); 
+                                this.closest('form').submit();"
+                                class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-indigo-500 hover:bg-white mt-4 lg:mt-0">
+                            {{ __('Log Out') }}
+                        </a>
+                    </form>
+                </div>
+            @else
+                <a href="{{ route('login') }}" class="block mt-4 lg:inline-block lg:mt-0 text-indigo-200 hover:text-white mr-4">Log in</a>
+
+                @if (Route::has('register'))
+                    <a href="{{ route('register') }}" class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-indigo-500 hover:bg-white mt-4 lg:mt-0">Register</a>
+                @endif
+            @endauth
+        @endif
     </div>
   </nav>
